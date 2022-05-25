ba7095389adcfea5472ee0576b61b71f118da80f
allity <allity@naver.com>
2022-05-26 01:07:41 +0900

    update

---
+++ antMan/antMan/antMan.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int box[10][10] = { 0 };
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            cin >> box[i][j];
+        }
+    }
+
+    int i = 1, j = 0;
+
+    while (true)
+    {
+        if (box[i][j + 1] == 2)
+        {
+            box[i][j + 1] = 9;
+            break;
+        }
+        else if (box[i][j + 1] == 0)
+        {
+            box[i][j + 1] = 9;
+            j++;
+        }
+        else
+        {
+            if (box[i + 1][j] == 2)
+            {
+                box[i + 1][j] = 9;
+                break;
+            }
+            else if (box[i + 1][j] == 0)
+            {
+                box[i + 1][j] = 9;
+                i++;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            cout << box[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
---
+++ test/test/test.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> car;
+
+    car.push_back(1);
+
+    cout << car[0];
+
+    return 0;
+}
